2df215373cf4bb94846d6d438058566ecdb85b19
Logan Gore <lgore@maana.io>
2019-11-06 11:24:41 -0800

    Initial template.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import AssistantAPIClient from '@io-maana/q-assistant-client'
+require('dotenv').config()
+
+console.log(
+  'Your example env. variable { REACT_APP_VAR } is set to: ',
+  process.env.REACT_APP_VAR
+)
+
+//
+// ----- Some quick queries to get you started ------
+//
+
+// Get the workspace.
+// const ws = await AssistantAPIClient.getWorkspace()
+
+// Get the workspace kinds.
+// const kinds = await ws.getKinds()
+
+// Get the workspace functions.
+// const funcs = await ws.getFunctions()
+
+// Get kind or function by ID.
+// const someKind = await AssistantAPIClient.getKindById('id....')
+// const someFunc = await AssistantAPIClient.getFunctionById('id....')
+
+// Get the active graph.
+// const ag = await ws.getActiveGraph()
+
+// Get the active graph's nodes.
+// const nodes = await ag.getNodes()
+
+async function addFunctionNode() {
+  const createFunctionInput = {
+    name: 'assistantGeneratedFunction',
+    arguments: [],
+    outputType: 'STRING',
+    graphqlOperationType: 'QUERY',
+    functionType: 'CKG'
+  }
+
+  const func = await AssistantAPIClient.createFunction(createFunctionInput)
+
+  const ws = await AssistantAPIClient.getWorkspace()
+  const ag = await ws.getActiveGraph()
+  const node = await ag.addNode('Function', func)
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1> Hello, Workspace!</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={() => {
+            addFunctionNode()
+          }}
         >
-          Learn React
-        </a>
+          Add a Function Node!
+        </button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
